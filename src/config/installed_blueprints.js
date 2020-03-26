0cabaf35474fcb334c0e0b812329ed24d56d35d7
Crowdbotics <team@crowdbotics.com>
2020-03-26 22:32:17 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Feed22930', human_name: 'Feed', access_route: 'Feed22930'},
 { name: 'Feed22929', human_name: 'Feed', access_route: 'Feed22929'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Feed22930Navigator from '../features/Feed22930/navigator';
 import Feed22929Navigator from '../features/Feed22929/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import TutorialNavigator from '../features/Tutorial/navigator';
@@ -22,6 +23,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Feed22930: { screen: Feed22930Navigator },
 Feed22929: { screen: Feed22929Navigator },
 Messenger: { screen: MessengerNavigator },
 Tutorial: { screen: TutorialNavigator },
